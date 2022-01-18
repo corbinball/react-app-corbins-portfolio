2f6c0143b69ae80e2d8f5b213e900e55476e1aef
Corbin Ball <c.ball@me.com>
2022-01-17 23:22:08 -0600

    edited error in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/PortfolioContainer";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <Header />;
+const App = () => <PortfolioContainer />;
 
 
 export default App;
