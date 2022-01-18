bd3ed8ea23f65cbc723de16b68d88c11bc67676c
Corbin Ball <c.ball@me.com>
2022-01-17 23:19:48 -0600

    added pages for portfolio info

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>
+     i will put my projects here
+      </p>
+    </div>
+  );
+}
