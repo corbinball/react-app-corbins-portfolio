e3df63c8e1f894f273b017d2b45d0b6ed59d2c58
Corbin Ball <c.ball@me.com>
2022-01-18 20:29:50 -0600

    made footer for page

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <h1>I MADE THIS:</h1>
+      <h2>
+       CORBIN BALL
+       </h2>
+    </footer>
+  );
+}
