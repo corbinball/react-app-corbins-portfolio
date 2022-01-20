3203c9047229eebd8bde757a59f80a2866243370
Corbin <c.ball@me.com>
2022-01-19 22:29:34 -0600

    complted my about me section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-       i will put about me here
-       </p>
-    </div>
+          <div id="About-Me">
+            <h2><span>About Me</span></h2>
+                <div>
+                    <img className= "width: 300px, height: auto, display: block, margin-left: auto, margin-right: auto"
+                        src="https://github.com/corbinball/corbins-portfolio/blob/main/assets/images/me.png?raw=true"
+                        alt= "Corbin Ball"/>
+                </div>
+                <div class="aboutmeinfo">
+                    <p><span>Lets keep this short and sweet. I am a new seed of a developer. <br>I will seek the sun and ask for water when I can't find any.</br>
+                        <br>My name is Corbin Ball and I am 33 years old.</br><br> I hope to develope from a small seed to a legendary tree, in a forests of other legends.</br>
+                        <br>LETS MAKE THIS GROWTH!</br>
+                        </span>
+                    </p>
+                </div>
+            </div>
   );
 }
