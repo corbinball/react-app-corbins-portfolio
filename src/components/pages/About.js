96963aa0adec0a33b4c8572be9f73a6c10c6f2c3
Corbin <c.ball@me.com>
2022-01-19 23:38:10 -0600

    fixed about me to show up by removing br and /br

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,20 +2,31 @@ import React from 'react';
 
 export default function About() {
   return (
-          <div id="About-Me">
+          <div>
             <h2><span>About Me</span></h2>
-                <div>
-                    <img className= "width: 300px, height: auto, display: block, margin-left: auto, margin-right: auto"
-                        src="https://github.com/corbinball/corbins-portfolio/blob/main/assets/images/me.png?raw=true"
+            <div id="About-Me">
+                <img className= "width: 25%, height: auto, display: block, margin-left: auto, margin-right: auto"
+                        src= "https://github.com/corbinball/corbins-portfolio/blob/main/assets/images/me.png?raw=true"
                         alt= "Corbin Ball"/>
                 </div>
-                <div class="aboutmeinfo">
-                    <p><span>Lets keep this short and sweet. I am a new seed of a developer. <br>I will seek the sun and ask for water when I can't find any.</br>
-                        <br>My name is Corbin Ball and I am 33 years old.</br><br> I hope to develope from a small seed to a legendary tree, in a forests of other legends.</br>
-                        <br>LETS MAKE THIS GROWTH!</br>
-                        </span>
-                    </p>
+                <div className="color: #215007;
+                            display: flex;
+                            max-width: 800px;
+                            margin: 0 auto;
+                            flex-wrap: wrap;
+                            justify-content: center;
+                            align-content: center;
+                            font-size: medium;
+                            line-height: 3.9;"
+                            >
+                    <p>Lets keep this short and sweet.</p>
+                    <p> I am a new seed of a developer.</p>
+                    <p> I will seek the sun and ask for water when I can't find any. </p>
+                    <p> My name is Corbin Ball and I am 33 years old.</p>
+                    <p> I hope to develope from a small seed to a legendary tree, in a forests of other legends.</p>
+                        <h1>LETS MAKE THIS GROWTH!</h1>
+                  
                 </div>
-            </div>
+          </div>
   );
 }
